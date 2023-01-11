319f678d790ebfa0d59a7bdee275eb3e616bce31
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-11 15:10:22 +0800

    added list to sidebar. changed footer text.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,8 +8,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
-
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -22,6 +21,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatListModule,
   ],
   exports: [
     HeaderComponent,
