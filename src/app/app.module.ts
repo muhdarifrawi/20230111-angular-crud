393458ccba36927ba9380ab6c51ca5686fd7bc06
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-11 14:47:04 +0800

    added new module and components. Exported to use on app.component.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
+import { LayoutModule } from './layout/layout.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
