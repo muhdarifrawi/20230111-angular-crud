7fdf9bdfdda1f951b22d9d97bf8e60026b9772a3
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-11 15:04:23 +0800

    generated new componenets in user.

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateUserComponent,
+    ViewUserComponent,
+    EditUserComponent,
+    DeleteUserComponent
+  ],
   imports: [
     CommonModule
   ]
