e901db878c243e0c8ee454739eda3b0758bdb83a
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-11 15:18:54 +0800

    added routes to app-routing.module.ts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { DeleteUserComponent } from './users/delete-user/delete-user.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+import { ListUserComponent } from './users/list-user/list-user.component';
+import { ViewUserComponent } from './users/view-user/view-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"create",
+    component:CreateUserComponent
+  },
+  {
+    path:"list",
+    component:ListUserComponent
+  },
+  {
+    path:"view",
+    component:ViewUserComponent
+  },
+  {
+    path:"edit",
+    component:EditUserComponent
+  },
+  {
+    path:"delete",
+    component:DeleteUserComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
