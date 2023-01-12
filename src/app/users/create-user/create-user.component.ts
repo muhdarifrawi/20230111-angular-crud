e373c051abc64acd55bd83e53911085049b9c444
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-12 14:56:04 +0800

    created base form submission without link to DB.

--- src/app/users/create-user/create-user.component.ts
+++ src/app/users/create-user/create-user.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit{
+  
+  // reactive form
+  addUserForm:FormGroup=new FormGroup({})
 
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(){
+    this.addUserForm=this.formBuilder.group({
+      "username":new FormControl(""),
+      "email":new FormControl(""),
+      "phone":new FormControl(""),
+    })
+  }
+
+  createUser(){
+    console.log("createUser triggered")
+    console.log(this.addUserForm.value)
+  }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -9,6 +9,14 @@ import { ListUserComponent } from './list-user/list-user.component';
 // material ui api
 import {MatCardModule} from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -16,11 +24,17 @@ import { RouterModule } from '@angular/router';
     ViewUserComponent,
     EditUserComponent,
     DeleteUserComponent,
-    ListUserComponent
+    ListUserComponent,
   ],
   imports: [
     CommonModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     RouterModule
   ]
 })
